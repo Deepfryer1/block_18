9ae27a514a5ef0bda578daa5c87a015db0224303
Deepfryer1 <ben.savage2@icloud.com>
2024-10-10 15:11:16 -0400

    Create block_18.js

---
+++ block_18.js
@@ -0,0 +1,17 @@
+// UNIT TESTS
+
+// Multiplication Function
+// Expect product(2,6) to be a number
+// Expect product(2,6) to be equal to 12
+// Expect product("b, 6) to be an error
+
+// Merging Arrays/ ConcatOdds Function
+// Expect mergingArrayReturningOdds to take the odd numbers and make an Array
+// Expect mergingArrayReturningOdds to show only one odd number and not multiple ones in Array
+// Expect originalArray to result in a prompt "Try Again!" and return to previous page if a string is provided
+
+// Functional Tests
+// When a user has completed putting together a shopping cart made up of items, they should have the option of checking out now or saving their cart.
+// If the shopping cart is empty, the user isn't prompted to do anything when the browser closes.
+// After the user reviews the shopping cart they are asked to check out as a guest or to login to their account.
+// The last prompt that displays is a final check about whether the user would like to change any quantity of the items or to save individual items for another time.
